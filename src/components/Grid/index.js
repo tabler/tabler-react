07e80ad451e790f23c6605b805d3754458366404
Jon Thompson <jon@jont.me>
2018-04-14 00:48:20 +0100

    Adds Grid.Col and Grid.Row

---
+++ src/components/Grid/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+import GridRow from "./GridRow";
+import GridCol from "./GridCol";
+
+const Grid = {
+  Row: GridRow,
+  Col: GridCol
+};
+
+export { Grid as default };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 // @flow
 
 export { default as Container } from "./Container";
+export { default as Grid } from "./Grid";
