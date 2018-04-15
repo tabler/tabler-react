10d8cac6aaf07352857055aa72d07af0a84d3e26
AaronCoplan <afcoplan@gmail.com>
2018-04-15 12:41:38 -0700

    Renamed App.js in example to App.react.js

--- example/src/index.js
+++ example/src/index.js
@@ -4,6 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './index.css'
-import App from './App'
+import App from './App.react'
 
 ReactDOM.render(<App />, document.getElementById('root'))
