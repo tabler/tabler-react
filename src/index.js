bdc45eb13987e579ab4c2aff4aadb44cf59549b6
AaronCoplan <afcoplan@gmail.com>
2018-04-14 23:38:43 -0700

    Adding prettier for auto formatting and precommit hook.

--- src/index.js
+++ src/index.js
@@ -2,25 +2,19 @@
  * @class ExampleComponent
  */
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styles from './styles.css'
+import styles from "./styles.css";
 
 export default class ExampleComponent extends Component {
   static propTypes = {
     text: PropTypes.string
-  }
+  };
 
   render() {
-    const {
-      text
-    } = this.props
+    const { text } = this.props;
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    return <div className={styles.test}>Example Component: {text}</div>;
   }
 }
