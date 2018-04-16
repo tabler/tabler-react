a05b7aca39b75e07a91c962a9b3fce89527b24fc
Jon Thompson <jonthomp@users.noreply.github.com>
2018-04-16 13:54:25 +0100

    Remove form to prevent clash

--- src/components/index.js
+++ src/components/index.js
@@ -13,5 +13,3 @@ export { default as Avatar } from "./Avatar";
 export { default as Badge } from "./Badge";
 export { default as Dropdown } from "./Dropdown";
 export { default as Button } from "./Button";
-
-export { default as Form } from "./Form";
