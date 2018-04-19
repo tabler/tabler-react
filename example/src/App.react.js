52db80645ea287133b126ecedea071c3684a7fa8
Greg Hamel <hamegreg@gmail.com>
2018-04-18 22:25:51 -0400

    Clean up after deletion.

--- example/src/App.react.js
+++ example/src/App.react.js
@@ -10,14 +10,12 @@ const {
   ForgotPasswordPage,
   LoginPage,
   RegisterPage,
-  FormElements,
   Error400,
   Error401,
   Error403,
   Error404,
   Error500,
   Error503,
-  Empty
 } = Pages;
 
 type Props = {||};
@@ -29,14 +27,12 @@ function App(props: Props): React.Node {
         <Route exact path="/forgot-password" component={ForgotPasswordPage} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/register" component={RegisterPage} />
-        <Route exact path="/forms-elements" component={FormElements} />
         <Route exact path="/400" component={Error400} />
         <Route exact path="/401" component={Error401} />
         <Route exact path="/403" component={Error403} />
         <Route exact path="/404" component={Error404} />
         <Route exact path="/500" component={Error500} />
         <Route exact path="/503" component={Error503} />
-        <Route exact path="/empty" component={Empty} />
         <Route path="*" exact={true} component={Error404} />
       </React.Fragment>
     </Router>
--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,8 +1,6 @@
 import ForgotPasswordPage from "./ForgotPasswordPage.react";
 import LoginPage from "./LoginPage.react";
 import RegisterPage from "./RegisterPage.react";
-import FormElements from "./FormElements.react";
-import Empty from "./Empty.react";
 import Error400 from "./400.react";
 import Error401 from "./401.react";
 import Error403 from "./403.react";
@@ -14,12 +12,10 @@ export default {
   ForgotPasswordPage,
   LoginPage,
   RegisterPage,
-  FormElements,
   Error400,
   Error401,
   Error403,
   Error404,
   Error500,
   Error503,
-  Empty
 };
