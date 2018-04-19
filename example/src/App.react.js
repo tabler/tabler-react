63e474295f3d6faea727730da1a599383fca484f
jonthomp <jon@jont.me>
2018-04-19 21:44:21 +0100

    Adds FormElements demo page to router

--- example/src/App.react.js
+++ example/src/App.react.js
@@ -14,6 +14,8 @@ import {
   Error503,
 } from "./pages";
 
+import FormElements from "./FormElements.react";
+
 import "bootstrap";
 
 import "./App.css";
@@ -34,6 +36,7 @@ function App(props: Props): React.Node {
         <Route exact path="/500" component={Error500} />
         <Route exact path="/503" component={Error503} />
         <Route path="*" exact={true} component={Error404} />
+        <Route exact path="/form-elements" component={FormElements} />
       </React.Fragment>
     </Router>
   );
