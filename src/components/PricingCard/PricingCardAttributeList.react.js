38c00c53078e55bfa4aeee58a7bd9020fe8f7bd3
burak akyol <burakakyol0795@gmail.com>
2018-04-21 01:32:29 +0300

    Added component props and view

--- src/components/PricingCard/PricingCardAttributeList.react.js
+++ src/components/PricingCard/PricingCardAttributeList.react.js
@@ -2,3 +2,23 @@
 /*
 Pricing Card Attribute List component
 */
+// IDEA: Adding ArrayOf<String | any> instead of children
+
+import * as React from "react";
+import cn from "classnames";
+
+type Props = {|
+  +children?: React.Node,
+  +className?: string,
+|};
+
+function PricingCardAttributeList({
+  className,
+  children,
+  transparent,
+}: Props): React.Node {
+  const classes = cn("list-unstyled", "leading-loose");
+  return <ul className={classes}>{children}</ul>;
+}
+
+export default PricingCardAttributeList;
