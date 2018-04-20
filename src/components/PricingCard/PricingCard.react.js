d0ae1c424382c1343aaa42d54d88ee486d90f04c
burak akyol <burakakyol0795@gmail.com>
2018-04-21 02:41:45 +0300

    Added PricingCard component

--- src/components/PricingCard/PricingCard.react.js
+++ src/components/PricingCard/PricingCard.react.js
@@ -1,4 +1,36 @@
-// @flow
-/*
-Pricing Card component
-*/
+//@flow
+
+import * as React from "react";
+
+import PricingCardCategory from "./PricingCardCategory.react";
+import PricingCardPrice from "./PricingCardPrice.react";
+import PricingCardAttributeList from "./PricingCardAttributeList.react";
+import PricingCardAttributeItem from "./PricingCardAttributeItem.react";
+import PricingCardButton from "./PricingCardButton.react";
+
+type Props = {|
+  +children?: React.Node,
+  +className?: string,
+  +active?: boolean,
+  +title?: string | React.Node,
+  +body?: React.Node,
+|};
+
+function PricingCard({
+  className,
+  children,
+  active = false,
+  title,
+  body,
+}: Props): React.Node {
+  // TODO: Add component logic.
+  return { children };
+}
+
+PricingCard.Category = PricingCardCategory;
+PricingCard.Price = PricingCardPrice;
+PricingCard.AttributeList = PricingCardAttributeList;
+PricingCard.AttributeItem = PricingCardAttributeItem;
+PricingCard.Button = PricingCardButton;
+
+export default PricingCard;
