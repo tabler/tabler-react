f5de78b580c67e477178609614bd3270a7794fb4
burak akyol <burakakyol0795@gmail.com>
2018-04-23 18:55:25 +0300

    Added component logic.

--- src/components/PricingCard/PricingCard.react.js
+++ src/components/PricingCard/PricingCard.react.js
@@ -1,6 +1,8 @@
 //@flow
 
 import * as React from "react";
+import cn from "classnames";
+import Card from "../Card/Card.react";
 
 import PricingCardCategory from "./PricingCardCategory.react";
 import PricingCardPrice from "./PricingCardPrice.react";
@@ -12,19 +14,29 @@ type Props = {|
   +children?: React.Node,
   +className?: string,
   +active?: boolean,
-  +title?: string | React.Node,
-  +body?: React.Node,
+  +category?: React.Node,
 |};
 
 function PricingCard({
   className,
   children,
   active = false,
-  title,
-  body,
+  category,
 }: Props): React.Node {
+  const cardBodyClassName = cn("text-center");
+  const cardStatusClassName = cn("card-status", "bg-green");
+  const cardStatus = <div className={cardStatusClassName} />;
+  const cardCategory = <PricingCardCategory>{category}</PricingCardCategory>;
   // TODO: Add component logic.
-  return { children };
+  return (
+    <Card>
+      {active && cardStatus}
+      <Card.Body className={cardBodyClassName}>
+        {category && cardCategory}
+        {children}
+      </Card.Body>
+    </Card>
+  );
 }
 
 PricingCard.Category = PricingCardCategory;
