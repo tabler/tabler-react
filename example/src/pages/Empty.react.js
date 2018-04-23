1cf28695387167c55bab4ba6792653a8745881a9
Greg Hamel <hamegreg@gmail.com>
2018-04-22 21:08:21 -0400

    Add Empty Page

---
+++ example/src/pages/Empty.react.js
@@ -0,0 +1,24 @@
+// @flow
+
+import React from "react";
+
+import {
+  Page,
+  Avatar,
+  Icon,
+  Grid,
+  Card,
+  Text,
+  Header,
+  Table,
+  Alert,
+  Progress,
+} from "tabler-react";
+
+import SiteWrapper from "../SiteWrapper.react";
+
+function Empty() {
+  return <SiteWrapper />;
+}
+
+export default Empty;
