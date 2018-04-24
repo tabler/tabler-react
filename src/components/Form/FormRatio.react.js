4886b2991d3c5fc0c77b435932ab1bf418c911c8
jonthomp <jon@jont.me>
2018-04-24 14:28:53 +0100

    Split types into Controlled | UnControlled, removes constructor, simplifies render

--- src/components/Form/FormRatio.react.js
+++ src/components/Form/FormRatio.react.js
@@ -4,56 +4,52 @@ import * as React from "react";
 import cn from "classnames";
 import { Grid } from "../";
 
-type Props = {|
+type PropsForAll = {|
   +className?: string,
   +step?: number,
   +min?: number,
   +max?: number,
-  +defaultValue?: number,
-  +value?: number,
-  +onChange?: (SyntheticInputEvent<EventTarget>) => mixed,
 |};
+
+type ControlledProps = {|
+  ...PropsForAll,
+  +value: number,
+  +onChange: (SyntheticInputEvent<EventTarget>) => mixed,
+|};
+
+type UnControlledProps = {|
+  ...PropsForAll,
+  +defaultValue: number,
+|};
+
+type Props = ControlledProps | UnControlledProps;
 
 type State = {|
   internalValue: number,
 |};
 
 class FormRatio extends React.PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    if (props.defaultValue && (props.value || props.onChange)) {
-      console.error(
-        `Warning Form.Ratio: You should not use defaultValue with value
-         and onChange if you want to control the input yourself`
-      );
-    }
+  state = {
+    internalValue: !this.props.onChange ? this.props.defaultValue : 0,
+  };
 
-    this.state = {
-      internalValue: props.defaultValue || props.value || 0,
-    };
-  }
-
-  handleOnChange = (e: SyntheticInputEvent<EventTarget>) => {
-    const { defaultValue, onChange } = this.props;
-    const value = Number(e.target.value);
-    if (typeof defaultValue !== "number") {
-      if (onChange) onChange(e);
+  handleOnChange = (e: SyntheticInputEvent<EventTarget>): mixed => {
+    if (this.props.onChange) {
+      this.props.onChange(e);
     } else {
+      const value = Number(e.target.value);
       this.setState({ internalValue: value });
     }
   };
 
   render(): React.Node {
-    const {
-      className,
-      step = 1,
-      min = 0,
-      max = 0,
-      defaultValue,
-      value,
-    } = this.props;
-    const { internalValue } = this.state;
+    const { className, step = 1, min = 0, max = 0 } = this.props;
     const classes = cn(className);
+
+    const value: number = this.props.onChange
+      ? this.props.value
+      : this.state.internalValue;
+
     return (
       <Grid.Row className={classes} alignItems="center">
         <Grid.Col>
@@ -64,14 +60,14 @@ class FormRatio extends React.PureComponent<Props, State> {
             min={min}
             max={max}
             onChange={this.handleOnChange}
-            value={typeof defaultValue === "number" ? internalValue : value}
+            value={value}
           />
         </Grid.Col>
         <Grid.Col auto>
           <input
             type="number"
             className="form-control w-8"
-            value={typeof defaultValue === "number" ? internalValue : value}
+            value={value}
             readOnly
           />
         </Grid.Col>
@@ -79,5 +75,7 @@ class FormRatio extends React.PureComponent<Props, State> {
     );
   }
 }
+
+FormRatio.displayName = "Form.Ratio";
 
 export default FormRatio;
