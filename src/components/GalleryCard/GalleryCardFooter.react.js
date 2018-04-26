6e08e832ae3a78fd13955d208a99c6b0a8c52d1c
Atölye15 <burakakyol0795@gmail.com>
2018-04-26 17:17:01 +0300

    Updated component logic

--- src/components/GalleryCard/GalleryCardFooter.react.js
+++ src/components/GalleryCard/GalleryCardFooter.react.js
@@ -1,14 +1,12 @@
 // @flow
-// GallerdCardFooter Component
 import * as React from "react";
 import cn from "classnames";
 
 type Props = {|
-  +className?: string,
   +children?: React.Node,
 |};
 
-function GalleryCardFooter({ className, children }: Props) {
+function GalleryCardFooter({ children }: Props) {
   const classes = cn("d-flex", "align-items-center", "px-2");
 
   return <div className={classes}>{children}</div>;
