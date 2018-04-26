612aa2f1893190985439e05701c5b6e8eb5823ca
jonthomp <jon@jont.me>
2018-04-26 22:51:43 +0100

    Adds docs wrapper, sidebar and into page

---
+++ example/src/documentation/DocsIntroPage.react.js
@@ -0,0 +1,44 @@
+// @flow
+import React, { Component } from "react";
+
+import { Text, Header, Card, Icon } from "tabler-react";
+
+import SyntaxHighlighter from "react-syntax-highlighter/prism";
+import { prism } from "react-syntax-highlighter/styles/prism";
+
+import DocsPageWrapper from "./DocsPageWrapper.react";
+import ComponentDemo from "../ComponentDemo";
+
+function DocsIntroPage() {
+  return (
+    <DocsPageWrapper>
+      <Header.H2>Introduction</Header.H2>
+      <p>A set of React components for Tabler.</p>
+      <p>Add to your project with npm or yarn:</p>
+      <SyntaxHighlighter language="bash" style={prism}>
+        {`npm install react-tabler-components --save
+
+or
+
+yarn add react-tabler-components`}
+      </SyntaxHighlighter>
+      <br />
+      <p>Import the components into your project and start building:</p>
+
+      <ComponentDemo>
+        <Card>
+          <Card.Header>
+            <Card.Title>Card Title</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Text color="red">
+              Some Red text next to an icon
+              <Icon name="user" />
+            </Text>
+          </Card.Body>
+        </Card>
+      </ComponentDemo>
+    </DocsPageWrapper>
+  );
+}
+export default DocsIntroPage;
---
+++ example/src/documentation/index.js
@@ -0,0 +1,5 @@
+import DocsIntroPage from "./DocsIntroPage.react";
+import DocsPageWrapper from "./DocsPageWrapper.react";
+import DocsSidebar from "./DocsSidebar.react";
+
+export { DocsIntroPage, DocsPageWrapper, DocsSidebar };
