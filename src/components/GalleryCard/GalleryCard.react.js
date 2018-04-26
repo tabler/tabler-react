8dddee371908899a8c14391b158cbcf566c29aea
Atölye15 <burakakyol0795@gmail.com>
2018-04-26 17:16:09 +0300

    Updated component logic

--- src/components/GalleryCard/GalleryCard.react.js
+++ src/components/GalleryCard/GalleryCard.react.js
@@ -1,9 +1,27 @@
 // @flow
-
+import * as React from "react";
+import cn from "classnames";
+import Card from "../Card";
 /*
 totalLikes
 totalViews
-avatar
+avatarURL
 dateString
 date
+userDisplayName
+imageURL
 */
+// TODO: ImagePosition:bottom | top
+
+type Props = {|
+  +children?: string,
+  +className?: string,
+  +imageURL?: string,
+|};
+
+function GalleryCard({ children, className, imageURL }: Props) {
+  const cardClassName = cn("p-3", className);
+  return <Card className={cardClassName}>{children}</Card>;
+}
+
+export default GalleryCard;
