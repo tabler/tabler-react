ea676d9234db49bbac36aa4b9481a9547e4c5748
jonthomp <jon@jont.me>
2018-04-27 14:42:16 +0100

    Adds state to handle fileName display

--- src/components/Form/FormFileInput.react.js
+++ src/components/Form/FormFileInput.react.js
@@ -13,30 +13,57 @@ type Props = {|
   +onChange?: (event: SyntheticInputEvent<HTMLInputElement>) => void,
 |};
 
-function FormFileInput({
-  className,
-  value,
-  name,
-  label = "Choose file",
-  disabled,
-  readOnly,
-  onChange,
-}: Props): React.Node {
-  const classes = cn("custom-file", className);
-  return (
-    <div className={classes}>
-      <input
-        type="file"
-        className="custom-file-input"
-        name={name}
-        value={value}
-        disabled={disabled}
-        readOnly={readOnly}
-        onChange={onChange}
-      />
-      <label class="custom-file-label">{label}</label>
-    </div>
-  );
+type State = {| fileName: string |};
+
+class FormFileInput extends React.Component<Props, State> {
+  state = {
+    fileName: "",
+  };
+
+  _handleOnChange = (event: SyntheticInputEvent<HTMLInputElement>): void => {
+    console.log(event.target.files);
+    this.setState({ fileName: event.target.files[0].name });
+    if (this.props.onChange) {
+      this.props.onChange(event);
+    }
+  };
+
+  render(): React.Node {
+    const {
+      className,
+      value,
+      name,
+      label: labelFromProps = "Choose file",
+      disabled,
+      readOnly,
+    } = this.props;
+
+    const classes = cn("custom-file", className);
+    const label = this.state.fileName || labelFromProps;
+    return (
+      <div className={classes}>
+        <input
+          type="file"
+          className="custom-file-input"
+          name={name}
+          value={value}
+          disabled={disabled}
+          readOnly={readOnly}
+          onChange={this._handleOnChange}
+        />
+        <label
+          className="custom-file-label"
+          style={{
+            whiteSpace: "nowrap",
+            display: "block",
+            overflow: "hidden",
+          }}
+        >
+          {label}
+        </label>
+      </div>
+    );
+  }
 }
 
 FormFileInput.displayName = "Form.FileInput";
