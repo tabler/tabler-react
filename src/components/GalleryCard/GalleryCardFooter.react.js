a771f478d240d4f0f777b9fdc405558a7035e53d
burak akyol <burakakyol0795@gmail.com>
2018-04-28 16:08:17 +0300

    Added displayName and component return type

--- src/components/GalleryCard/GalleryCardFooter.react.js
+++ src/components/GalleryCard/GalleryCardFooter.react.js
@@ -6,10 +6,12 @@ type Props = {|
   +children?: React.Node,
 |};
 
-function GalleryCardFooter({ children }: Props) {
+function GalleryCardFooter({ children }: Props):React.Node {
   const classes = cn("d-flex", "align-items-center", "px-2");
 
   return <div className={classes}>{children}</div>;
 }
+
+GalleryCardFooter.displayName="GalleryCard.Footer";
 
 export default GalleryCardFooter;
