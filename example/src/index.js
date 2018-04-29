f14a7f6187efbe1caa4ff5e5b70489873da73041
jonthomp <jon@jont.me>
2018-04-29 15:07:26 +0100

    Add react-c3js

--- example/src/index.js
+++ example/src/index.js
@@ -4,6 +4,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import "c3/c3.css";
+
 import App from "./App.react";
 
 const rootElement = document.getElementById("root");
