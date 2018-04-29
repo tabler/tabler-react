54d1ce6c46e57807e82c616a9b5c5b99dd8751d5
jonthomp <jon@jont.me>
2018-04-29 12:09:46 +0100

    Adds alignContent props to Table.Col and Table.ColHeader

--- src/components/Table/TableCol.react.js
+++ src/components/Table/TableCol.react.js
@@ -3,15 +3,19 @@
 import * as React from "react";
 import cn from "classnames";
 
-type Props = {| +children?: React.Node, +className?: string |};
+type Props = {|
+  +children?: React.Node,
+  +className?: string,
+  +alignContent?: "left" | "center" | "right",
+|};
 
-function TableCol({ className, children, ...props }: Props): React.Node {
-  const classes = cn(className);
-  return (
-    <td className={classes} {...props}>
-      {children}
-    </td>
-  );
+function TableCol({
+  className,
+  children,
+  alignContent = "",
+}: Props): React.Node {
+  const classes = cn({ [`text-${alignContent}`]: alignContent }, className);
+  return <td className={classes}>{children}</td>;
 }
 
 TableCol.displayName = "Table.Col";
