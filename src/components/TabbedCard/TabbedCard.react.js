5282ceb919123958bec4fb40e865cf985f186e32
AaronCoplan <afcoplan@gmail.com>
2018-05-04 05:51:13 +0000

    Added basic styled TabbedCard.

--- src/components/TabbedCard/TabbedCard.react.js
+++ src/components/TabbedCard/TabbedCard.react.js
@@ -2,6 +2,8 @@
 
 import * as React from "react";
 
+import "./TabbedCard.css";
+
 import Card from "../Card/Card.react";
 
 type Props = {||};
@@ -10,7 +12,24 @@ type State = {||};
 
 class TabbedCard extends React.PureComponent<Props, State> {
   render(): React.Node {
-    return <Card />;
+    return (
+      <Card>
+        <div className="card-header">
+          <ul className="nav nav-tabs TabbedCard_header_tabs">
+            <li className="nav-item">
+              <a className="nav-link active">Active</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Disabled</a>
+            </li>
+          </ul>
+        </div>
+        <Card.Body>Hello World</Card.Body>
+      </Card>
+    );
   }
 }
 
