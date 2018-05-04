6477f25676b58636e72d6be839ad603271db4c5a
AaronCoplan <afcoplan@gmail.com>
2018-05-04 05:40:12 +0000

    Added default TabbedCard as basic card.

--- src/components/TabbedCard/TabbedCard.react.js
+++ src/components/TabbedCard/TabbedCard.react.js
@@ -1,1 +1,17 @@
 // @flow
+
+import * as React from "react";
+
+import Card from "../Card/Card.react";
+
+type Props = {||};
+
+type State = {||};
+
+class TabbedCard extends React.PureComponent<Props, State> {
+  render(): React.Node {
+    return <Card />;
+  }
+}
+
+export default TabbedCard;
