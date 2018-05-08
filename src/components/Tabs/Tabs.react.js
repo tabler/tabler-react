93d85ba86cabdc79e3c2b6575e86294e9ba592ae
AaronCoplan <afcoplan@gmail.com>
2018-05-08 08:44:37 +0000

    Added props and state definition.

--- src/components/Tabs/Tabs.react.js
+++ src/components/Tabs/Tabs.react.js
@@ -2,13 +2,18 @@
 
 import * as React from "react";
 
-//import Tab from "./Tab.react";
+import Tab from "./Tab.react";
 //import TabbedContainer from "./TabbedContainer.react";
 //import TabbedHeader from "./TabbedHeader.react";
 
-type Props = {||};
+type Props = {|
+  +initialTab: string,
+  +children: React.ChildrenArray<React.Element<typeof Tab>>,
+|};
 
-type State = {||};
+type State = {|
+  selectedTitle: string,
+|};
 
 class Tabs extends React.PureComponent<Props, State> {}
 
