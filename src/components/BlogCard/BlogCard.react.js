8af033c33f1cf0e2de6771ed93a83603d07d0922
khaldiAmer <khaldi.ameur1995@gmail.com>
2018-05-19 15:50:48 +0100

    Added blog card component. #62

---
+++ src/components/BlogCard/BlogCard.react.js
@@ -0,0 +1,111 @@
+//@flow
+
+import * as React from "react";
+import Card from "../Card/Card.react";
+import Icon from "../Icon/Icon.react";
+
+type Props = {|
+  +children?: React.Node,
+  +title?: string,
+  +titleUrl?: string,
+  +avatarUrl?: string,
+  +description?: string,
+  +date?: string,
+  +imgUrl?: string,
+  +iconName?: string,
+  +iconHref?: string,
+  +authorName?: string,
+  +avatarImgSrc?: string,
+  +imgSrc?: string,
+  +imgAlt?: string,
+  +aside?: boolean,
+  +postHref?: string,
+|};
+
+function BlogCard({
+  children,
+  title,
+  description,
+  avatarUrl,
+  imgUrl,
+  imgAlt,
+  aside,
+  authorName,
+  date,
+  imgSrc = " ",
+  avatarImgSrc = " ",
+  iconName,
+  iconHref,
+  postHref,
+}: Props): React.Node {
+  return !aside ? (
+    <Card>
+      <a href={postHref}>
+        <img
+          className="card-img-top"
+          src={imgSrc}
+          alt="And this isn't my nose. This is a false one."
+        />
+      </a>
+      <Card.Body className="d-flex flex-column">
+        <h4>
+          {" "}
+          <a href={postHref}>{title}</a>
+        </h4>
+        <div className="text-muted">{description}</div>
+        <div className="d-flex align-items-center pt-5 mt-auto">
+          <div
+            className="avatar avatar-md mr-3"
+            style={{ backgroundImage: `url(${avatarImgSrc}` }}
+          />
+          <div>
+            <a href="./profile.html" className="text-default">
+              {authorName}
+            </a>
+            <small className="d-block text-muted">{date}</small>
+          </div>
+          <div className="ml-auto text-muted">
+            <a href={iconHref} className="icon d-none d-md-inline-block ml-3">
+              <Icon prefix="fe" name={iconName || "heart"} />
+            </a>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  ) : (
+    <Card className="card-aside">
+      <a
+        href={postHref}
+        className="card-aside-column"
+        style={{ backgroundImage: `url(${imgSrc})` }}
+      >
+        {" "}
+      </a>
+      <Card.Body className="d-flex flex-column">
+        <h4>
+          <a href={postHref}>{title}</a>
+        </h4>
+        <div className="text-muted">{description}</div>
+        <div className="d-flex align-items-center pt-5 mt-auto">
+          <div
+            className="avatar avatar-md mr-3"
+            style={{ backgroundImage: `url(${avatarImgSrc}` }}
+          />
+          <div>
+            <a href="./profile.html" className="text-default">
+              {authorName}
+            </a>
+            <small className="d-block text-muted">{date}</small>
+          </div>
+          <div className="ml-auto text-red">
+            <a href={iconHref} className="icon d-none d-md-inline-block ml-3">
+              <Icon prefix="fe" name={iconName || "heart"} />
+            </a>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BlogCard;
---
+++ src/components/BlogCard/README.md
@@ -0,0 +1,28 @@
+## Store Component
+
+### Usage
+
+```jsx
+import { BlogCard } from 'tabler-react'
+
+...
+...
+...
+
+
+< BlogCard
+  aside
+  title=""
+  titleUrl=""
+  description=""
+  avatarUrl=""
+  authorName=""
+  authorUrl=""
+  date=""
+  imgUrl=""
+  imgAlt=""
+  iconName=""
+  iconHref=""
+
+/>
+```
