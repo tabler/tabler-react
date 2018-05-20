51c20f613ec0b3c8b4b76b92c35f847f6d007cc7
Jon Thompson <jon@iu-hq.com>
2018-05-20 19:19:03 +0100

    fix(Dropdown): Fix Dropdown should when user clicks outside of it
    Creates a new ClickOutside helper component and uses it in Dropdown and NavItem
    
    Fixes #165

---
+++ src/helpers/ClickOutside.react.js
@@ -0,0 +1,44 @@
+// @flow
+
+import * as React from "react";
+
+type Props = {|
+  +children: ({| +setElementRef: (el: ?HTMLElement) => mixed |}) => React.Node,
+  +onOutsideClick: () => void,
+|};
+
+/**
+ * A helper to help you do something when a user clicks outside of a component
+ */
+class ClickOutside extends React.PureComponent<Props, void> {
+  elementRef: ?HTMLElement;
+
+  componentDidMount = () => {
+    document.addEventListener("mousedown", this.handleOutsideOnClick, false);
+  };
+
+  componentWillUnmount = () => {
+    document.removeEventListener("mousedown", this.handleOutsideOnClick, false);
+  };
+
+  setElementRef = (el: ?HTMLElement): mixed => {
+    if (el) this.elementRef = el;
+  };
+
+  isOutsideClick = (target: mixed) =>
+    this.elementRef &&
+    target instanceof Node &&
+    !this.elementRef.contains(target);
+
+  handleOutsideOnClick: MouseEventListener = ({ target }) => {
+    if (this.isOutsideClick(target)) this.props.onOutsideClick();
+    return;
+  };
+
+  render() {
+    const { children } = this.props;
+    return children({ setElementRef: this.setElementRef });
+  }
+}
+
+export default ClickOutside;
