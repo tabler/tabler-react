b4b5e335534b60a68d9df8fbd7fda7f65407821f
Jon Thompson <jon@iu-hq.com>
2018-05-21 21:05:07 +0100

    feat(SiteFooter): Make the SiteFooter content editable via props
    Resolves #177

--- src/components/Site/SiteFooter.react.js
+++ src/components/Site/SiteFooter.react.js
@@ -1,108 +1,94 @@
 // @flow
 
 import * as React from "react";
-import { Container, Grid, List, Button } from "../../components";
+import { Container, Grid, List } from "../../components";
 
-const SiteFooter = () => (
+export type Props = {|
+  /**
+   * An array of the 8 links displayed in the first footer bar
+   */
+  +links?: Array<React.Node>,
+  /**
+   * The text block displayed next to the links
+   */
+  +note?: string,
+  /**
+   * The content of the very bottom copyright notice space
+   */
+  +copyright?: React.Node,
+  /**
+   * The content of the very bottom nav space
+   */
+  +nav?: React.Node,
+|};
+
+/**
+ * The footer of your website
+ */
+const SiteFooter = ({
+  links = [],
+  note,
+  copyright,
+  nav,
+}: Props): React.Node => (
   <React.Fragment>
-    <div className="footer">
-      <Container>
-        <Grid.Row>
-          <Grid.Col lg={8}>
-            <Grid.Row>
-              <Grid.Col width={6} md={3}>
-                <List unstyled={true} className="mb-0">
-                  <List.Item>
-                    <a>First link</a>
-                  </List.Item>
-                  <List.Item>
-                    <a>Second link</a>
-                  </List.Item>
-                </List>
-              </Grid.Col>
-              <Grid.Col width={6} md={3}>
-                <List unstyled={true} className="mb-0">
-                  <List.Item>
-                    <a>Third link</a>
-                  </List.Item>
-                  <List.Item>
-                    <a>Fourth link</a>
-                  </List.Item>
-                </List>
-              </Grid.Col>
-              <Grid.Col width={6} md={3}>
-                <List unstyled={true} className="mb-0">
-                  <List.Item>
-                    <a>Fifth link</a>
-                  </List.Item>
-                  <List.Item>
-                    <a>Sixth link</a>
-                  </List.Item>
-                </List>
-              </Grid.Col>
-              <Grid.Col width={6} md={3}>
-                <List unstyled={true} className="mb-0">
-                  <List.Item>
-                    <a>Other link</a>
-                  </List.Item>
-                  <List.Item>
-                    <a>Last link</a>
-                  </List.Item>
-                </List>
-              </Grid.Col>
-            </Grid.Row>
-          </Grid.Col>
-          <Grid.Col lg={4} className="mt-4 mt-lg-0">
-            Premium and Open Source dashboard template with responsive and high
-            quality UI. For Free!
-          </Grid.Col>
-        </Grid.Row>
-      </Container>
-    </div>
-    <footer className="footer">
-      <Container>
-        <Grid.Row className="align-items-center flex-row-reverse">
-          <Grid.Col auto={true} className="ml-auto">
-            <Grid.Row className="align-items-center">
-              <Grid.Col auto={true}>
-                <List className="list-inline list-inline-dots mb-0">
-                  <List.Item className="list-inline-item">
-                    <a href="./docs/index.html">Documentation</a>
-                  </List.Item>
-                  <List.Item className="list-inline-item">
-                    <a href="./faq.html">FAQ</a>
-                  </List.Item>
-                </List>
-              </Grid.Col>
-              <Grid.Col auto={true}>
-                <Button
-                  href="https://github.com/tabler/tabler-react"
-                  size="sm"
-                  outline
-                  color="primary"
-                  RootComponent="a"
-                >
-                  Source code
-                </Button>
-              </Grid.Col>
-            </Grid.Row>
-          </Grid.Col>
-          <Grid.Col width={12} lgAuto className="mt-3 mt-lg-0 text-center">
-            Copyright © 2018
-            <a href="."> Tabler-react</a>. Theme by
-            <a
-              href="https://codecalm.net"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              codecalm.net
-            </a>{" "}
-            All rights reserved.
-          </Grid.Col>
-        </Grid.Row>
-      </Container>
-    </footer>
+    {(links || note) && (
+      <div className="footer">
+        <Container>
+          <Grid.Row>
+            <Grid.Col lg={8}>
+              <Grid.Row>
+                {links && (
+                  <React.Fragment>
+                    <Grid.Col width={6} md={3}>
+                      <List unstyled={true} className="mb-0">
+                        <List.Item>{links[0]}</List.Item>
+                        <List.Item>{links[1]}</List.Item>
+                      </List>
+                    </Grid.Col>
+                    <Grid.Col width={6} md={3}>
+                      <List unstyled={true} className="mb-0">
+                        <List.Item>{links[2]}</List.Item>
+                        <List.Item>{links[3]}</List.Item>
+                      </List>
+                    </Grid.Col>
+                    <Grid.Col width={6} md={3}>
+                      <List unstyled={true} className="mb-0">
+                        <List.Item>{links[4]}</List.Item>
+                        <List.Item>{links[5]}</List.Item>
+                      </List>
+                    </Grid.Col>
+                    <Grid.Col width={6} md={3}>
+                      <List unstyled={true} className="mb-0">
+                        <List.Item>{links[6]}</List.Item>
+                        <List.Item>{links[7]}</List.Item>
+                      </List>
+                    </Grid.Col>
+                  </React.Fragment>
+                )}
+              </Grid.Row>
+            </Grid.Col>
+            <Grid.Col lg={4} className="mt-4 mt-lg-0">
+              {note}
+            </Grid.Col>
+          </Grid.Row>
+        </Container>
+      </div>
+    )}
+    {(nav || copyright) && (
+      <footer className="footer">
+        <Container>
+          <Grid.Row className="align-items-center flex-row-reverse">
+            <Grid.Col auto={true} className="ml-auto">
+              <Grid.Row className="align-items-center">{nav}</Grid.Row>
+            </Grid.Col>
+            <Grid.Col width={12} lgAuto className="mt-3 mt-lg-0 text-center">
+              {copyright}
+            </Grid.Col>
+          </Grid.Row>
+        </Container>
+      </footer>
+    )}
   </React.Fragment>
 );
 
--- src/components/Site/SiteWrapper.react.js
+++ src/components/Site/SiteWrapper.react.js
@@ -5,18 +5,21 @@ import * as React from "react";
 import { Page, Site } from "../";
 import type { Props as SiteHeaderProps } from "./SiteHeader.react";
 import type { Props as SiteNavProps } from "./SiteNav.react";
+import type { Props as SiteFooterProps } from "./SiteFooter.react";
 
 type Props = {|
   +headerProps: SiteHeaderProps,
   +navProps: SiteNavProps,
+  +footerProps: SiteFooterProps,
   +children: React.Node,
 |};
 
 function SiteWrapper(props: Props): React.Node {
-  const { headerProps, navProps, children } = props;
+  const { headerProps, navProps, footerProps, children } = props;
 
   const header = React.createElement(Site.Header, headerProps);
   const nav = React.createElement(Site.Nav, navProps);
+  const footer = React.createElement(Site.Footer, footerProps);
 
   return (
     <Page>
@@ -25,7 +28,7 @@ function SiteWrapper(props: Props): React.Node {
         {nav}
         {children}
       </Page.Main>
-      <Site.Footer />
+      {footer}
     </Page>
   );
 }
