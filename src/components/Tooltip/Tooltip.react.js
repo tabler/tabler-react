90dca549cf5b5c2c0d826d9102c00ab6dcbe5a90
Greg Hamel <hamegreg@gmail.com>
2018-05-24 21:51:37 -0400

    feat(tooltip): add new tooltip component
    adds new tooltip component to tabler-react

---
+++ src/components/Tooltip/Tooltip.react.js
@@ -0,0 +1,79 @@
+// @flow
+
+import * as React from "react";
+import cn from "classnames";
+import { Manager, Placement, Reference, Popper } from "react-popper";
+import type { PopperChildrenProps, ReferenceChildrenProps } from "react-popper";
+
+type Props = {|
+  +content: string,
+  +children?: React.Node,
+  +className?: string,
+  +placement?: Placement,
+|};
+
+type State = {
+  isShown: boolean,
+};
+
+class Tooltip extends React.Component<Props, State> {
+  state = { isShown: false };
+
+  _handleTriggerOnMouseEnter = (e: SyntheticMouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    this.setState({ isShown: true });
+  };
+
+  _handleTriggerOnMouseLeave = (e: SyntheticMouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    this.setState({ isShown: false });
+  };
+
+  render(): React.Node {
+    const { className, children, placement, content } = this.props;
+
+    const classes = cn(
+      "tooltip",
+      placement ? "bs-tooltip-" + placement : null,
+      {
+        show: this.state.isShown,
+      },
+      className
+    );
+
+    return (
+      <Manager>
+        <Reference>
+          {({ ref }: ReferenceChildrenProps) => (
+            <div
+              ref={ref}
+              onMouseEnter={this._handleTriggerOnMouseEnter}
+              onMouseLeave={this._handleTriggerOnMouseLeave}
+            >
+              {children}
+            </div>
+          )}
+        </Reference>
+        {this.state.isShown ? (
+          <Popper placement={placement} eventsEnabled={true}>
+            {({ ref, style, placement }: PopperChildrenProps) => {
+              return (
+                <div
+                  className={classes}
+                  data-placement={placement}
+                  style={style}
+                  ref={ref}
+                >
+                  {/* <div className="arrow tbr-arrow" /> */}
+                  <div className="tooltip-inner">{content}</div>
+                </div>
+              );
+            }}
+          </Popper>
+        ) : null}
+      </Manager>
+    );
+  }
+}
+
+export default Tooltip;
