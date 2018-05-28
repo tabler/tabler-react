b533b0d37c2f85e4e5c7a8c26785b8be730090cb
Jon Thompson <jon@iu-hq.com>
2018-05-28 17:16:32 +0100

    refactor(FormCheckbox): export props type

--- src/components/Form/FormCheckbox.react.js
+++ src/components/Form/FormCheckbox.react.js
@@ -4,7 +4,7 @@ import * as React from "react";
 import cn from "classnames";
 import Form from "./";
 
-type Props = {|
+export type Props = {|
   +className?: string,
   +label?: string,
   +value?: string | number | boolean,
