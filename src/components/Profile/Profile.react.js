fd907fb4f1c214375cf59033d579c8bdd69ea22a
Jon Thompson <jon@iu-hq.com>
2018-06-12 14:15:48 +0100

    feat(Profile): add second layout option

--- src/components/Profile/Profile.react.js
+++ src/components/Profile/Profile.react.js
@@ -3,45 +3,87 @@
 import * as React from "react";
 import cn from "classnames";
 
-import { Card, Header, SocialNetworksList } from "../../components";
+import {
+  Card,
+  Header,
+  SocialNetworksList,
+  Media,
+  Avatar,
+} from "../../components";
 
 import ProfileImage from "./ProfileImage.react";
 
-export type Props = {|
+import type { itemObject } from "../SocialNetworksList/SocialNetworksList.react";
+
+export type PropsForAll = {|
   +children?: React.Node,
   +className?: string,
   +name: string,
   +avatarURL?: string,
   +twitterURL?: string,
+  +facebookURL?: string,
+  +socialNetworksObjects?: Array<itemObject>,
+|};
+
+type layout1 = {|
+  ...PropsForAll,
+  +layout?: 1,
   +backgroundURL?: string,
   +bio?: string,
 |};
 
-function Profile({
-  className,
-  children,
-  name,
-  avatarURL = "",
-  twitterURL = "",
-  backgroundURL = "",
-  bio,
-}: Props): React.Node {
-  const classes = cn("card-profile", className);
-  return (
-    <Card className={classes}>
-      <Card.Header backgroundURL={backgroundURL} />
-      <Card.Body className="text-center">
-        <ProfileImage avatarURL={avatarURL} />
-        <Header.H3 className="mb-3">{name}</Header.H3>
-        <p className="mb-4">{bio || children}</p>
-        <SocialNetworksList
-          itemsObjects={[{ name: "twitter", label: "Follow" }]}
-          prefix="fa"
-          asButtons
-        />
-      </Card.Body>
-    </Card>
-  );
+type layout2 = {|
+  ...PropsForAll,
+  +layout: 2,
+  +jobTitle?: string,
+|};
+
+type Props = layout1 | layout2;
+
+function Profile(props: Props): React.Node {
+  const {
+    className,
+    children,
+    name,
+    avatarURL = "",
+    socialNetworksObjects,
+  } = props;
+  const classes = cn({ "card-profile": props.layout === 1 }, className);
+  if (!props.layout || props.layout === 1) {
+    const { bio, backgroundURL = "" } = props;
+    return (
+      <Card className={classes}>
+        <Card.Header backgroundURL={backgroundURL} />
+        <Card.Body className="text-center">
+          <ProfileImage avatarURL={avatarURL} />
+          <Header.H3 className="mb-3">{name}</Header.H3>
+          <p className="mb-4">{bio || children}</p>
+          <SocialNetworksList
+            itemsObjects={socialNetworksObjects}
+            prefix="fa"
+            asButtons
+          />
+        </Card.Body>
+      </Card>
+    );
+  }
+  if (props.layout === 2) {
+    const { jobTitle = "" } = props;
+    return (
+      <Card className={classes}>
+        <Card.Body>
+          <Media>
+            <Avatar size="xxl" className="mr-5" imageURL={avatarURL} />
+            <Media.BodySocial
+              name={name}
+              workTitle={jobTitle}
+              itemsObjects={socialNetworksObjects}
+            />
+          </Media>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 Profile.Image = ProfileImage;
