8a9a3040f4cdbb4b168d084508569161944711aa
Jon Thompson <jon@iu-hq.com>
2018-06-20 22:18:29 +0100

    docs(): wrap react router NavLinks in withRouter

--- example/src/SiteWrapper.react.js
+++ example/src/SiteWrapper.react.js
@@ -1,7 +1,7 @@
 // @flow
 
 import * as React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 
 import { Site, Nav, Grid, List, Button } from "tabler-react";
 
@@ -26,56 +26,77 @@ type navItem = {|
 |};
 
 const navBarItems: Array<navItem> = [
-  { value: "Home", to: "/", icon: "home", LinkComponent: NavLink },
+  { value: "Home", to: "/", icon: "home", LinkComponent: withRouter(NavLink) },
   {
     value: "Interface",
     icon: "box",
     subItems: [
-      { value: "Cards Design", to: "/cards", LinkComponent: NavLink },
-      { value: "Charts", to: "/charts", LinkComponent: NavLink },
-      { value: "Pricing Cards", to: "/pricing-cards", LinkComponent: NavLink },
+      {
+        value: "Cards Design",
+        to: "/cards",
+        LinkComponent: withRouter(NavLink),
+      },
+      { value: "Charts", to: "/charts", LinkComponent: withRouter(NavLink) },
+      {
+        value: "Pricing Cards",
+        to: "/pricing-cards",
+        LinkComponent: withRouter(NavLink),
+      },
     ],
   },
   {
     value: "Components",
     icon: "calendar",
     subItems: [
-      { value: "Maps", to: "/maps", LinkComponent: NavLink },
-      { value: "Icons", to: "/icons", LinkComponent: NavLink },
-      { value: "Store", to: "/store", LinkComponent: NavLink },
-      { value: "Blog", to: "/blog", LinkComponent: NavLink },
+      { value: "Maps", to: "/maps", LinkComponent: withRouter(NavLink) },
+      { value: "Icons", to: "/icons", LinkComponent: withRouter(NavLink) },
+      { value: "Store", to: "/store", LinkComponent: withRouter(NavLink) },
+      { value: "Blog", to: "/blog", LinkComponent: withRouter(NavLink) },
     ],
   },
   {
     value: "Pages",
     icon: "file",
     subItems: [
-      { value: "Profile", to: "/profile", LinkComponent: NavLink },
-      { value: "Login", to: "/login", LinkComponent: NavLink },
-      { value: "Register", to: "/register", LinkComponent: NavLink },
+      { value: "Profile", to: "/profile", LinkComponent: withRouter(NavLink) },
+      { value: "Login", to: "/login", LinkComponent: withRouter(NavLink) },
+      {
+        value: "Register",
+        to: "/register",
+        LinkComponent: withRouter(NavLink),
+      },
       {
         value: "Forgot password",
         to: "/forgot-password",
-        LinkComponent: NavLink,
+        LinkComponent: withRouter(NavLink),
       },
-      { value: "400 error", to: "/400", LinkComponent: NavLink },
-      { value: "401 error", to: "/401", LinkComponent: NavLink },
-      { value: "403 error", to: "/403", LinkComponent: NavLink },
-      { value: "404 error", to: "/404", LinkComponent: NavLink },
-      { value: "500 error", to: "/500", LinkComponent: NavLink },
-      { value: "503 error", to: "/503", LinkComponent: NavLink },
-      { value: "Email", to: "/email", LinkComponent: NavLink },
-      { value: "Empty page", to: "/empty-page", LinkComponent: NavLink },
-      { value: "RTL", to: "/rtl", LinkComponent: NavLink },
+      { value: "400 error", to: "/400", LinkComponent: withRouter(NavLink) },
+      { value: "401 error", to: "/401", LinkComponent: withRouter(NavLink) },
+      { value: "403 error", to: "/403", LinkComponent: withRouter(NavLink) },
+      { value: "404 error", to: "/404", LinkComponent: withRouter(NavLink) },
+      { value: "500 error", to: "/500", LinkComponent: withRouter(NavLink) },
+      { value: "503 error", to: "/503", LinkComponent: withRouter(NavLink) },
+      { value: "Email", to: "/email", LinkComponent: withRouter(NavLink) },
+      {
+        value: "Empty page",
+        to: "/empty-page",
+        LinkComponent: withRouter(NavLink),
+      },
+      { value: "RTL", to: "/rtl", LinkComponent: withRouter(NavLink) },
     ],
   },
   {
     value: "Forms",
     to: "/form-elements",
     icon: "check-square",
-    LinkComponent: NavLink,
+    LinkComponent: withRouter(NavLink),
   },
-  { value: "Gallery", to: "/gallery", icon: "image", LinkComponent: NavLink },
+  {
+    value: "Gallery",
+    to: "/gallery",
+    icon: "image",
+    LinkComponent: withRouter(NavLink),
+  },
   {
     icon: "file-text",
     value: "Documentation",
