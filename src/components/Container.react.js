1a2ea4b4ca5c38cac8d6982150b55e0179432c49
Alan Bueno <alanbueno8@gmail.com>
2018-06-30 18:10:58 -0300

    feat(Container): container optional className prop
    Container capability to receive className prop

--- src/components/Container.react.js
+++ src/components/Container.react.js
@@ -1,13 +1,16 @@
 // @flow
 
 import * as React from "react";
+import cn from "classnames";
 
 type Props = {|
   +children?: React.Node,
+  +className?: string,
 |};
 
-function Container(props: Props): React.Node {
-  return <div className={"container"}>{props.children}</div>;
+function Container({ className, children }: Props): React.Node {
+  const classes = cn("container", className);
+  return <div className={classes}>{children}</div>;
 }
 
 export default Container;
