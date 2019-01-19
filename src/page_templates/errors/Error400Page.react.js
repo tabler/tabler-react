85d6150c717f1b8def6db8ee9d109e6e7cf31e17
Thomas Smyth <thomas.smyth3141@gmail.com>
2019-01-19 11:36:10 +0000

    Error Page Action

--- src/page_templates/errors/Error400Page.react.js
+++ src/page_templates/errors/Error400Page.react.js
@@ -9,9 +9,15 @@ function Error400Page({
   title = "400",
   subtitle = "Oops... You just found an error page...",
   details = "We are sorry but your request contains bad syntax and cannot be fulfilled...",
+  action,
 }: DefaultErrorProps): React.Node {
   return (
-    <DefaultErrorPage title={title} subtitle={subtitle} details={details} />
+    <DefaultErrorPage
+      title={title}
+      subtitle={subtitle}
+      details={details}
+      action={action}
+    />
   );
 }
 
--- src/page_templates/errors/Error401Page.react.js
+++ src/page_templates/errors/Error401Page.react.js
@@ -9,9 +9,15 @@ function Error401Page({
   title = "401",
   subtitle = "Oops... You just found an error page...",
   details = "We are sorry but you are not authorized to access this page...",
+  action,
 }: DefaultErrorProps): React.Node {
   return (
-    <DefaultErrorPage title={title} subtitle={subtitle} details={details} />
+    <DefaultErrorPage
+      title={title}
+      subtitle={subtitle}
+      details={details}
+      action={action}
+    />
   );
 }
 
