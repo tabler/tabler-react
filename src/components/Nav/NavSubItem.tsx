5b158cf94c907d52478faffb2f4a073af28b4dff
Jon Thompson <jon@jont.me>
2019-06-01 18:22:39 +0100

    chore(TS): convert Nav

--- src/components/Nav/NavSubItem.tsx
+++ src/components/Nav/NavSubItem.tsx
@@ -1,15 +1,14 @@
-// @flow
 import * as React from "react";
 import Dropdown from "../Dropdown";
 
-type Props = {|
-  +children?: React.Node,
-  +className?: string,
-  +LinkComponent?: React.ElementType,
-  +to?: string,
-  +icon?: string,
-  +value?: string,
-|};
+type Props = {
+  children?: React.ReactNode;
+  className?: string;
+  LinkComponent?: React.ElementType;
+  to?: string;
+  icon?: string;
+  value?: string;
+};
 
 function NavSubItem({
   children,
@@ -17,9 +16,8 @@ function NavSubItem({
   className,
   to,
   icon,
-  hasSubNav,
   value,
-}: Props): React.Node {
+}: Props) {
   return (
     <Dropdown.Item to={to} icon={icon} RootComponent={LinkComponent}>
       {value || children}
--- src/components/Nav/NavSubmenu.tsx
+++ src/components/Nav/NavSubmenu.tsx
@@ -1,14 +1,12 @@
-// @flow
-
 import * as React from "react";
 import cn from "classnames";
 
-type Props = {|
-  +children?: React.Node,
-  +className?: string,
-|};
+interface Props {
+  children?: React.ReactNode;
+  className?: string;
+}
 
-function NavSubmenu({ className, children }: Props): React.Node {
+function NavSubmenu({ className, children }: Props) {
   const classes = cn({ nav: true, "nav-submenu": true }, className);
   return <div className={classes}>{children}</div>;
 }
