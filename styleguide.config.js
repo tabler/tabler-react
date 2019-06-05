b33b8d790d46d6c154a18aed30167997f96b503c
Jon Thompson <jon@jont.me>
2019-06-05 12:09:01 +0100

    chore(docs): docs working with typescript
    This commit gets the styleguidist docs build script working. However, some components within the generated documentation are not rendering, the error is 'SyntaxError: Missing initializer in const declaration' I've not yet been able to solve it.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,8 +1,54 @@
 module.exports = {
-  components: "src/components/**/*.react.{js,jsx}",
-  webpackConfig: require("./example/node_modules/react-scripts/config/webpack.config.dev.js"),
+  components: "src/**/*.{tsx,ts,js}",
+  ignore: ["**/*.stories.*", "**/Code/*", "**/index.ts"],
+  propsParser: require("react-docgen-typescript").withDefaultConfig().parse,
+  webpackConfig: {
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    module: {
+      rules: [
+        // Babel loader, will use your project’s babel.config.js
+        {
+          test: /\.(ts|tsx|jsx|js)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        // Other loaders that are needed for your components
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "static/fonts",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[id].[ext]",
+                outputPath: "static/media",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
   getExampleFilename(componentPath) {
-    return componentPath.replace(/\.react.js?$/, ".examples.md");
+    return componentPath.replace(/\.tsx?$/, ".examples.md");
   },
   require: ["./src/Tabler.css"],
   styleguideDir: "./example/public/documentation",
@@ -46,151 +92,151 @@ module.exports = {
       sections: [
         {
           name: "Account Dropdown",
-          components: "src/components/AccountDropdown/**/*.react.{js,jsx}",
+          components: "src/components/AccountDropdown/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Alert",
-          components: "src/components/Alert/**/*.react.{js,jsx}",
+          components: "src/components/Alert/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Avatar",
-          components: "src/components/Avatar/**/*.react.{js,jsx}",
+          components: "src/components/Avatar/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Badge",
-          components: "src/components/Badge/**/*.react.{js,jsx}",
+          components: "src/components/Badge/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "BlogCard",
-          components: "src/components/BlogCard/**/*.react.{js,jsx}",
+          components: "src/components/BlogCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Button",
-          components: "src/components/Button/**/*.react.{js,jsx}",
+          components: "src/components/Button/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Card",
-          components: "src/components/Card/**/*.react.{js,jsx}",
+          components: "src/components/Card/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "ContactCard",
-          components: "src/components/ContactCard/**/*.react.{js,jsx}",
+          components: "src/components/ContactCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Container",
-          components: "src/components/Container.react.{js,jsx}",
+          components: "src/components/Container.{js,jsx,ts,tsx}",
         },
         {
           name: "Dimmer",
-          components: "src/components/Dimmer/**/*.react.{js,jsx}",
+          components: "src/components/Dimmer/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Dropdown",
-          components: "src/components/Dropdown/**/*.react.{js,jsx}",
+          components: "src/components/Dropdown/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Form",
-          components: "src/components/Form/**/*.react.{js,jsx}",
+          components: "src/components/Form/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "GalleryCard",
-          components: "src/components/GalleryCard/**/*.react.{js,jsx}",
+          components: "src/components/GalleryCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Grid",
-          components: "src/components/Grid/**/*.react.{js,jsx}",
+          components: "src/components/Grid/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Header",
-          components: "src/components/Header/**/*.react.{js,jsx}",
+          components: "src/components/Header/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Icon",
-          components: "src/components/Icon/**/*.react.{js,jsx}",
+          components: "src/components/Icon/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "List",
-          components: "src/components/List/**/*.react.{js,jsx}",
+          components: "src/components/List/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Loader",
-          components: "src/components/Loader/**/*.react.{js,jsx}",
+          components: "src/components/Loader/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Media",
-          components: "src/components/Media/**/*.react.{js,jsx}",
+          components: "src/components/Media/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Nav",
-          components: "src/components/Nav/**/*.react.{js,jsx}",
+          components: "src/components/Nav/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Notification",
-          components: "src/components/Notification/**/*.react.{js,jsx}",
+          components: "src/components/Notification/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Page",
-          components: "src/components/Page/**/*.react.{js,jsx}",
+          components: "src/components/Page/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Pricing Card",
-          components: "src/components/PricingCard/**/*.react.{js,jsx}",
+          components: "src/components/PricingCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Profile",
-          components: "src/components/Profile/**/*.react.{js,jsx}",
+          components: "src/components/Profile/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Progress Indicator",
-          components: "src/components/Progress/**/*.react.{js,jsx}",
+          components: "src/components/Progress/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Progress Card",
-          components: "src/components/ProgressCard/**/*.react.{js,jsx}",
+          components: "src/components/ProgressCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Social Networks List",
-          components: "src/components/SocialNetworksList/**/*.react.{js,jsx}",
+          components: "src/components/SocialNetworksList/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Site",
-          components: "src/components/Site/**/*.react.{js,jsx}",
+          components: "src/components/Site/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Stamp",
-          components: "src/components/Stamp/**/*.react.{js,jsx}",
+          components: "src/components/Stamp/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "StampCard",
-          components: "src/components/StampCard/**/*.react.{js,jsx}",
+          components: "src/components/StampCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "StatsCard",
-          components: "src/components/StatsCard/**/*.react.{js,jsx}",
+          components: "src/components/StatsCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "StoreCard",
-          components: "src/components/StoreCard/**/*.react.{js,jsx}",
+          components: "src/components/StoreCard/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "TabbedCard",
-          components: "src/components/{TabbedCard,Tabs}/**/*.react.{js,jsx}",
+          components: "src/components/{TabbedCard,Tabs}/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Table",
-          components: "src/components/Table/**/*.react.{js,jsx}",
+          components: "src/components/Table/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Tag",
-          components: "src/components/Tag/**/*.react.{js,jsx}",
+          components: "src/components/Tag/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Text",
-          components: "src/components/Text/**/*.react.{js,jsx}",
+          components: "src/components/Text/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Tooltip",
-          components: "src/components/Tooltip/**/*.react.{js,jsx}",
+          components: "src/components/Tooltip/**/*.{js,jsx,ts,tsx}",
         },
       ],
     },
@@ -199,21 +245,22 @@ module.exports = {
       sections: [
         {
           name: "Login Page",
-          components: "src/page_templates/account/LoginPage/*.react.{js,jsx}",
+          components:
+            "src/page_templates/account/LoginPage/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Register Page",
           components:
-            "src/page_templates/account/RegisterPage/*.react.{js,jsx}",
+            "src/page_templates/account/RegisterPage/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Forgot Password Page",
           components:
-            "src/page_templates/account/ForgotPasswordPage/*.react.{js,jsx}",
+            "src/page_templates/account/ForgotPasswordPage/**/*.{js,jsx,ts,tsx}",
         },
         {
           name: "Errors",
-          components: "src/page_templates/errors/**/*.react.{js,jsx}",
+          components: "src/page_templates/errors/**/*.{js,jsx,ts,tsx}",
         },
       ],
     },
