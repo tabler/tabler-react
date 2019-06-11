619154a3af0baddfef753788520e42abb3d2583c
Jon Thompson <jon@jont.me>
2019-06-11 22:21:23 +0100

    chore: delete default welcome story

--- src/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { storiesOf } from "@storybook/react";
-
-import { Text } from ".";
-
-storiesOf("Welcome", module).add("to Storybook", () => <Text>Welcome</Text>);
