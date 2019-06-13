14a6dc4e986f138162d3718db4670ca2f3816887
Jon Thompson <jon@jont.me>
2019-06-13 11:41:39 +0100

    fix(TablerComponent): add ReatNode as chldren type

--- src/types/TablerComponent.ts
+++ src/types/TablerComponent.ts
@@ -4,7 +4,7 @@ import React, { CSSProperties, ReactChild } from "react";
  * Base Props Interface for Tabler components
  */
 export interface TablerComponent {
-  children?: React.ReactChild | React.ReactChild[];
+  children?: React.ReactNode | React.ReactChild | React.ReactChild[];
   className?: string;
   style?: CSSProperties;
 }
