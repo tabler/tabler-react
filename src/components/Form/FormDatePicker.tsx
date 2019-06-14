aefe5acca0b4d303fdb14d2ea4bcfb79ee95a197
Jon Thompson <jon@jont.me>
2019-06-14 10:21:26 +0100

    refactor(FormDatePicker): convert to function

--- src/components/Form/FormDatePicker.tsx
+++ src/components/Form/FormDatePicker.tsx
@@ -1,56 +1,33 @@
-import * as React from "react";
-import FormSelect from "./FormSelect";
+import React, { useState } from "react";
+import FormSelect, { FormSelectProps } from "./FormSelect";
 import FormInputGroup from "./FormInputGroup";
 
-interface Props {
-  children?: React.ReactNode;
-  className?: string;
+export interface FormDatePickerProps
+  extends Pick<FormSelectProps, Exclude<keyof FormSelectProps, "onChange">> {
   defaultDate: Date;
   minYear: number;
   maxYear: number;
   format: string;
   monthLabels: Array<string>;
-  onChange?: (date: Date) => void;
-}
-
-interface State {
-  currentDate: Date;
+  onChange?: (value: Date) => void;
 }
 
 type ChangeTypes = "mm" | "yyyy" | "dd";
 
 type DateComponents = { [Key in ChangeTypes]: React.ReactNode };
 
-class FormDatePicker extends React.PureComponent<Props, State> {
-  static defaultProps = {
-    monthLabels: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    minYear: 1897,
-    maxYear: new Date().getFullYear(),
-    format: "mm/dd/yyyy",
-    defaultDate: new Date(),
-  };
-
-  state = {
-    currentDate: this.props.defaultDate,
-  };
+const FormDatePicker = function({
+  onChange,
+  monthLabels,
+  minYear,
+  maxYear,
+  format,
+  className,
+}: FormDatePickerProps) {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date(Date.now()));
 
   // Handle date changes
-  _handleOnChange = (type: ChangeTypes, value: number): void => {
-    const { currentDate } = this.state;
-    const { onChange } = this.props;
+  const _handleOnChange = (type: ChangeTypes, value: number): void => {
     const newDate: Date = new Date(currentDate);
 
     // Change month
@@ -66,23 +43,20 @@ class FormDatePicker extends React.PureComponent<Props, State> {
     if (type === "yyyy") {
       newDate.setFullYear(value);
     }
-
-    this.setState({ currentDate: newDate }, () => {
-      onChange && onChange(this.state.currentDate);
-    });
+    setCurrentDate(currentDate);
+    if (onChange) {
+      onChange(newDate);
+    }
   };
 
   // Creates an array with numeric values from start to end
-  _range = (start: number, end: number): Array<number> =>
+  const _range = (start: number, end: number): Array<number> =>
     Array.from({ length: end + 1 - start }, (v, k) => k + start);
 
   // Renders the months select
-  _renderMonths = () => {
-    const { currentDate } = this.state;
-    const { monthLabels } = this.props;
-
-    const onChangeMonths = (e: React.ChangeEvent<HTMLInputElement>): void =>
-      this._handleOnChange("mm", Number(e.target.value));
+  const _renderMonths = () => {
+    const onChangeMonths = (e: React.ChangeEvent<HTMLSelectElement>): void =>
+      _handleOnChange("mm", Number(e.target.value));
 
     return (
       <FormSelect onChange={onChangeMonths}>
@@ -100,18 +74,17 @@ class FormDatePicker extends React.PureComponent<Props, State> {
   };
 
   // Renders the days select
-  _renderDays = () => {
-    const { currentDate } = this.state;
+  const _renderDays = () => {
     const currentMonthDays = new Date(
       currentDate.getUTCFullYear(),
       currentDate.getUTCMonth() + 1,
       0
     ).getDate();
-    const daysRange = this._range(1, currentMonthDays);
+    const daysRange = _range(1, currentMonthDays);
     const currentDay = currentDate.getUTCDate();
 
-    const onChangeDays = (e: React.ChangeEvent<HTMLInputElement>) =>
-      this._handleOnChange("dd", Number(e.target.value));
+    const onChangeDays = (e: React.ChangeEvent<HTMLSelectElement>) =>
+      _handleOnChange("dd", Number(e.target.value));
 
     return (
       <FormSelect onChange={onChangeDays}>
@@ -125,14 +98,12 @@ class FormDatePicker extends React.PureComponent<Props, State> {
   };
 
   // renderes the years select
-  _renderYears = () => {
-    const { minYear, maxYear } = this.props;
-    const { currentDate } = this.state;
-    const yearsRange = this._range(minYear, maxYear).reverse();
+  const _renderYears = () => {
+    const yearsRange = _range(minYear, maxYear).reverse();
     const currentYear = currentDate.getUTCFullYear();
 
-    const onChangeYears = (e: React.ChangeEvent<HTMLInputElement>) =>
-      this._handleOnChange("yyyy", Number(e.target.value));
+    const onChangeYears = (e: React.ChangeEvent<HTMLSelectElement>) =>
+      _handleOnChange("yyyy", Number(e.target.value));
 
     return (
       <FormSelect onChange={onChangeYears}>
@@ -145,28 +116,43 @@ class FormDatePicker extends React.PureComponent<Props, State> {
     );
   };
 
-  render() {
-    const { format, className } = this.props;
-    const formatSplit = format.split("/");
-    const dateComponents: DateComponents = {
-      mm: this._renderMonths(),
-      dd: this._renderDays(),
-      yyyy: this._renderYears(),
-    };
+  const formatSplit = format.split("/");
+  const dateComponents: DateComponents = {
+    mm: _renderMonths(),
+    dd: _renderDays(),
+    yyyy: _renderYears(),
+  };
 
-    return (
-      <div className={className}>
-        <FormInputGroup>
-          {formatSplit.map(
-            (type: string): React.ReactNode =>
-              dateComponents[type as ChangeTypes]
-          )}
-        </FormInputGroup>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={className}>
+      <FormInputGroup>
+        {formatSplit.map(
+          (type: string): React.ReactNode => dateComponents[type as ChangeTypes]
+        )}
+      </FormInputGroup>
+    </div>
+  );
+};
 
-(FormDatePicker as any).displayName = "Form.DatePicker";
+FormDatePicker.defaultProps = {
+  monthLabels: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  minYear: 1897,
+  maxYear: new Date().getFullYear(),
+  format: "mm/dd/yyyy",
+  defaultDate: new Date(),
+};
 
 export default FormDatePicker;
