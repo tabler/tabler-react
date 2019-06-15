3cc4a5c4a47bc19d8c89f1454b4650f48c4a8d78
Jon Thompson <jon@jont.me>
2019-06-15 02:37:18 +0100

    refactor: Page

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,21 +1,20 @@
 import * as React from "react";
 import cn from "classnames";
+import { ELProps } from "../../helpers/makeHtmlElement";
+import El from "../El/El";
 
-type Props = {
-  children?: React.ReactNode;
-  className?: string;
-};
+export interface PageProps extends ELProps<HTMLDivElement> {}
 
-function Page({ className, children }: Props) {
+function Page({ className, children, ...rest }: PageProps) {
   const classes = cn("page d-flex h-auto min-h-screen", className);
   return (
-    <div className={classes}>
+    <El.Div className={classes} {...rest}>
       <div className="d-flex flex-fill">
         <div className="main-content flex-fill d-flex flex-column max-w-full">
           {children}
         </div>
       </div>
-    </div>
+    </El.Div>
   );
 }
 
