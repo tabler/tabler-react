b4c7efa6cafab700cc1504c29d8255c45b5390d0
Jon Thompson <jon@jont.me>
2019-06-15 02:38:11 +0100

    refactor: Tab components

--- src/components/Tabs/TabbedHeader.tsx
+++ src/components/Tabs/TabbedHeader.tsx
@@ -1,30 +1,36 @@
 import * as React from "react";
 import { Props as TabProps } from "./Tab";
 import Nav from "../Nav";
+import { ELProps } from "../../helpers/makeHtmlElement";
+import El from "../El/El";
 
-interface Props {
+export interface TabbedHeaderProps extends ELProps<HTMLUListElement> {
   children: React.ReactElement<TabProps>[] | React.ReactElement<TabProps>;
   selectedTitle: string;
   stateCallback: (selectedTitle: string) => void;
 }
 
-function TabbedHeader(props: Props) {
-  const { children, stateCallback } = props;
+function TabbedHeader({
+  children,
+  stateCallback,
+  selectedTitle,
+  ...rest
+}: TabbedHeaderProps) {
   const tabs = React.Children.toArray(children);
   return (
-    <ul className="nav nav-tabs Tab_header_tabs">
-      {tabs.map((tab, index) => {
-        const title = tab.props.title;
+    <El.Ul classNames="nav nav-tabs Tab_header_tabs" {...rest}>
+      {tabs.map(({ props: { title, ...tabRest } }, index) => {
         return (
           <Nav.Item
             key={index}
             value={title}
             onClick={() => stateCallback(title)}
-            active={title === props.selectedTitle}
+            active={title === selectedTitle}
+            {...tabRest}
           />
         );
       })}
-    </ul>
+    </El.Ul>
   );
 }
 
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 
 import { Props as TabProps } from "./Tab";
 import TabbedContainer from "./TabbedContainer";
@@ -6,40 +6,28 @@ import TabbedHeader from "./TabbedHeader";
 
 import "./Tabs.css";
 
-interface Props {
+export interface TabsProps {
   initialTab: string;
   children: React.ReactElement<TabProps>[] | React.ReactElement<TabProps>;
 }
 
-interface State {
-  selectedTitle: string;
-}
+const Tabs = function({ children, initialTab }: TabsProps) {
+  const [selectedTitle, setSelectedTitle] = useState(initialTab);
 
-class Tabs extends React.PureComponent<Props, State> {
-  state = {
-    selectedTitle: this.props.initialTab,
-  };
-
-  render() {
-    const { children } = this.props;
-    const { selectedTitle } = this.state;
-    return (
-      <React.Fragment>
-        <TabbedHeader
-          selectedTitle={selectedTitle}
-          stateCallback={(newTitle: string) =>
-            this.setState({ selectedTitle: newTitle })
-          }
-        >
-          {children}
-        </TabbedHeader>
-        <div className={"margin-bottom-24"} />
-        <TabbedContainer selectedTitle={selectedTitle}>
-          {children}
-        </TabbedContainer>
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <TabbedHeader
+        selectedTitle={selectedTitle}
+        stateCallback={setSelectedTitle}
+      >
+        {children}
+      </TabbedHeader>
+      <div className={"margin-bottom-24"} />
+      <TabbedContainer selectedTitle={selectedTitle}>
+        {children}
+      </TabbedContainer>
+    </React.Fragment>
+  );
+};
 
 export default Tabs;
