ba0985e8c50d0270f49312e6cc1ed6d41c86d006
Jon Thompson <jon@jont.me>
2019-06-21 00:00:26 +0100

    fix(withDropdownProvider): forward the ref

--- src/components/Dropdown/withDropdownProvider.tsx
+++ src/components/Dropdown/withDropdownProvider.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import DropdownProvider from "./DropdownProvider";
 
-export const withDropdownProvider = (Component: React.ComponentType) => (
-  props: any
-) => {
-  return (
-    <DropdownProvider>
-      <Component {...props} />
-    </DropdownProvider>
-  );
-};
+export const withDropdownProvider = (Component: React.ComponentType) =>
+  forwardRef((props: any, ref: React.Ref<any>) => {
+    return (
+      <DropdownProvider>
+        <Component ref={ref} {...props} />
+      </DropdownProvider>
+    );
+  });
 
 export default withDropdownProvider;
