f8d6ccb1bf3f7d5996d6856074ae81e7f11d1ac1
Jon Thompson <jon@jont.me>
2019-06-21 11:10:04 +0100

    types: omit the as prop with ref

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,7 @@ export interface ReactHTMLElementWithoutRef<T extends HTMLElement>
   extends Omit<React.HTMLProps<React.ReactHTMLElement<T>>, "ref"> {}
 
 export interface HTMLPropsWithoutRef<T extends HTMLElement>
-  extends Omit<React.HTMLProps<T>, "ref"> {}
+  extends Omit<React.HTMLProps<T>, "ref" | "as"> {}
 
 export interface ReactHTMLElementProps<T extends HTMLElement>
   extends React.HTMLProps<React.ReactHTMLElement<T>> {}
