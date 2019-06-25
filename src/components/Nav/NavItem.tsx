957a8b0a9e7c32780b4984e147cd6871a7385b41
Jon Thompson <jon@jont.me>
2019-06-26 00:51:39 +0100

    types: remove forwardRef generics

--- src/components/Nav/NavItem.tsx
+++ src/components/Nav/NavItem.tsx
@@ -64,7 +64,7 @@ export interface NavItemProps extends TablerComponent {
 export const NavItem: React.RefForwardingComponent<
   any,
   NavItemProps
-> = forwardRef<any, NavItemProps>(function(
+> = forwardRef(function(
   {
     children,
     LinkComponent,
