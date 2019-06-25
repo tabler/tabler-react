ebd45b20a77f4f4aee9c6350d2e8ebaa1f7c1050
Jon Thompson <jon@jont.me>
2019-06-25 01:55:15 +0100

    chore: export individual components

--- src/components/El/El.tsx
+++ src/components/El/El.tsx
@@ -1,49 +1,49 @@
 import makeElement from "../../helpers/makeHtmlElement";
 
-const Div = makeElement<HTMLDivElement>("div");
-const Span = makeElement<HTMLDivElement>("span");
+export const Div = makeElement<HTMLDivElement>("div");
+export const Span = makeElement<HTMLDivElement>("span");
 
-const Header = makeElement<HTMLDivElement>("header");
-const Section = makeElement<HTMLDivElement>("section");
-const Nav = makeElement<HTMLDivElement>("nav");
-const Footer = makeElement<HTMLDivElement>("footer");
+export const Header = makeElement<HTMLDivElement>("header");
+export const Section = makeElement<HTMLDivElement>("section");
+export const Nav = makeElement<HTMLDivElement>("nav");
+export const Footer = makeElement<HTMLDivElement>("footer");
 
-const A = makeElement<HTMLAnchorElement>("a");
-const Button = makeElement<HTMLButtonElement>("button");
-const P = makeElement<HTMLParagraphElement>("p");
+export const A = makeElement<HTMLAnchorElement>("a");
+export const Button = makeElement<HTMLButtonElement>("button");
+export const P = makeElement<HTMLParagraphElement>("p");
 
-const Ul = makeElement<HTMLUListElement>("ul");
-const Li = makeElement<HTMLLIElement>("li");
-const Ol = makeElement<HTMLOListElement>("ol");
+export const Ul = makeElement<HTMLUListElement>("ul");
+export const Li = makeElement<HTMLLIElement>("li");
+export const Ol = makeElement<HTMLOListElement>("ol");
 
-const H1 = makeElement<HTMLHeadingElement>("h1");
-const H2 = makeElement<HTMLHeadingElement>("h2");
-const H3 = makeElement<HTMLHeadingElement>("h3");
-const H4 = makeElement<HTMLHeadingElement>("h4");
-const H5 = makeElement<HTMLHeadingElement>("h5");
-const H6 = makeElement<HTMLHeadingElement>("h6");
+export const H1 = makeElement<HTMLHeadingElement>("h1");
+export const H2 = makeElement<HTMLHeadingElement>("h2");
+export const H3 = makeElement<HTMLHeadingElement>("h3");
+export const H4 = makeElement<HTMLHeadingElement>("h4");
+export const H5 = makeElement<HTMLHeadingElement>("h5");
+export const H6 = makeElement<HTMLHeadingElement>("h6");
 
-const Form = makeElement<HTMLFormElement>("form");
-const Input = makeElement<HTMLInputElement>("input");
-const Textarea = makeElement<HTMLTextAreaElement>("textarea");
-const Select = makeElement<HTMLSelectElement>("select");
-const Option = makeElement<HTMLOptionElement>("option");
-const Fieldset = makeElement<HTMLFieldSetElement>("fieldset");
-const Label = makeElement<HTMLLabelElement>("label");
+export const Form = makeElement<HTMLFormElement>("form");
+export const Input = makeElement<HTMLInputElement>("input");
+export const Textarea = makeElement<HTMLTextAreaElement>("textarea");
+export const Select = makeElement<HTMLSelectElement>("select");
+export const Option = makeElement<HTMLOptionElement>("option");
+export const Fieldset = makeElement<HTMLFieldSetElement>("fieldset");
+export const Label = makeElement<HTMLLabelElement>("label");
 
-const Iframe = makeElement<HTMLIFrameElement>("iframe");
+export const Iframe = makeElement<HTMLIFrameElement>("iframe");
 // const Svg = makeElement<SVGElement>("svg");
 
-const Small = makeElement<HTMLDivElement>("small");
+export const Small = makeElement<HTMLDivElement>("small");
 
-const Table = makeElement<HTMLTableElement>("table");
-const Thead = makeElement<HTMLTableSectionElement>("thead");
-const Tbody = makeElement<HTMLTableSectionElement>("tbody");
-const Tfoot = makeElement<HTMLTableSectionElement>("tfoot");
-const Col = makeElement<HTMLTableColElement>("col");
-const Tr = makeElement<HTMLTableRowElement>("tr");
-const Td = makeElement<HTMLTableCellElement>("td");
-const Th = makeElement<HTMLTableHeaderCellElement>("th");
+export const Table = makeElement<HTMLTableElement>("table");
+export const Thead = makeElement<HTMLTableSectionElement>("thead");
+export const Tbody = makeElement<HTMLTableSectionElement>("tbody");
+export const Tfoot = makeElement<HTMLTableSectionElement>("tfoot");
+export const Col = makeElement<HTMLTableColElement>("col");
+export const Tr = makeElement<HTMLTableRowElement>("tr");
+export const Td = makeElement<HTMLTableCellElement>("td");
+export const Th = makeElement<HTMLTableHeaderCellElement>("th");
 
 export const El = {
   Div,
