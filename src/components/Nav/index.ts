a8c558da6daf5047e5e77c054d3e2987e2089341
Jon Thompson <jon@jont.me>
2019-06-25 02:08:07 +0100

    fix: missing components and unused imports

--- src/components/Nav/index.ts
+++ src/components/Nav/index.ts
@@ -1,14 +1,12 @@
-// @flow
+import Nav from "./Nav";
 
-import Nav, { NavProps } from "./Nav";
-
-import NavItem, { NavItemProps } from "./NavItem";
-import NavLink, { NavLinkProps } from "./NavLink";
-import NavSubItem, { NavSubItemProps } from "./NavSubItem";
-import NavSubmenu, { NavSubmenuProps } from "./NavSubmenu";
-import NavSubmenuItem, { NavSubmenuItemProps } from "./NavSubmenuItem";
-import NavLinkTitle, { NavLinkTitleProps } from "./NavLinkTitle";
-import { ReactElement, ReactType, ElementType } from "react";
+import NavItem from "./NavItem";
+import NavLink from "./NavLink";
+import NavSubItem from "./NavSubItem";
+import NavSubmenu from "./NavSubmenu";
+import NavSubmenuItem from "./NavSubmenuItem";
+import NavLinkTitle from "./NavLinkTitle";
+import NavSubNav from "./NavSubNav";
 
 const CompoundNav = Nav as typeof Nav & {
   Item: typeof NavItem;
@@ -17,6 +15,7 @@ const CompoundNav = Nav as typeof Nav & {
   LinkTitle: typeof NavLinkTitle;
   Submenu: typeof NavSubmenu;
   SubmenuItem: typeof NavSubmenuItem;
+  SubNav: typeof NavSubNav;
 };
 
 (CompoundNav as React.FunctionComponent).displayName = "Nav";
@@ -27,14 +26,17 @@ CompoundNav.Link = NavLink;
 CompoundNav.LinkTitle = NavLinkTitle;
 CompoundNav.Submenu = NavSubmenu;
 CompoundNav.SubmenuItem = NavSubmenuItem;
+CompoundNav.SubNav = NavSubNav;
 
-export * from "./NavItem";
+// export * from "./NavItem";
 export {
   CompoundNav as default,
   CompoundNav as Nav,
+  NavItem,
   NavSubItem,
   NavLink,
   NavLinkTitle,
   NavSubmenu,
   NavSubmenuItem,
+  NavSubNav,
 };
